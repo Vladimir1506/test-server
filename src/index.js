5f7c51dc9c779f09b5397f12bf6ab3afe243be87
Vladimir Kurochkin <kuro4king@gmail.com>
2025-06-12 17:50:15 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import express, {Request, Response} from 'express'
+// import express, {Request, Response} from 'express'
+const express =require('express')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello !')
+app.get('/', (req, res) => {
+    res.send('Hello!-  !')
 })
 
 app.listen(port, () => {
